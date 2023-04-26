ed218cc96fed32ad1a767a8a131da483ddfc2af6
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-04-26 23:48:05 +0600

    set router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Components/Home.jsx'
 import './index.css'
-
+import About from './Components/About.jsx'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Home></Home>
+  },
+  {
+    path:'/about',
+    element:<About></About>
+  },
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}></RouterProvider>
 )
