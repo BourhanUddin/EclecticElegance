6de1c5fa2cb8222fddc3a75b4ba762d3bb0eafcb
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-04-26 22:21:16 +0600

    set Header,Footer,Home,About and Errorpage components

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='divide-y  text-gray-800'>
+        <div></div>
+        <div className='py-6 text-sm text-center text-gray-400'>
+          © 2023 HeroGadget Inc. All rights reserved.
+        </div>
+      </footer>
+    );
+};
+
+export default Footer;
